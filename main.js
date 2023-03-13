71e152af631b43508bf3cc2f8d2f3ab5409c07f1
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-03-13 15:59:21 +0100

    vue app title

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const { createApp } = Vue;
+
+// Vue App - Title
+const vm = createApp({
+    data() {
+        return {
+            title: 'Titolo grande'
+        };
+    }
+}).mount('#app');
